3ae7b23786bfc492be3faaf328bdd4baafeb7976
Zeeshan Khalid <zeeshankhalid.2k01@gmail.com>
2024-02-04 00:05:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def generate_audio():
     return send_file('generated_audio.mp3', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+        app.run(host='0.0.0.0', port=80, debug=True)
+
